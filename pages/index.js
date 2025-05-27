634842beb40a4a4efcf3705fa012ccd1b65ba408
yiwei <willyos0113@gmail.com>
2025-05-27 17:10:56 +0800

    購票頁: 完成 step1 -> step2， js 部分還未撰寫

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-// nav 導覽列
-$(".navbar-burger").on("click", (e) => {
-  $(e.currentTarget).toggleClass("is-active");
-  $(".navbar-menu").toggleClass("is-active");
-});
-$(".navbar-item button").hover((e) => {
-  console.log($(e.target));
-  $(e.target).toggleClass("is-focused");
-});
---
+++ pages/js/bookStep.js
@@ -0,0 +1,16 @@
+// nav 導覽列
+$(".navbar-burger").on("click", (e) => {
+  $(e.currentTarget).toggleClass("is-active");
+  $(".navbar-menu").toggleClass("is-active");
+});
+$(".navbar-item button").hover((e) => {
+  $(e.target).toggleClass("is-focused");
+});
+
+// section 主內容
+$(".back").on("click", () => {
+  location.href = "https://www.google.com";
+});
+$(".next").on("click", () => {
+  location.href = "bookStep2.html";
+});
