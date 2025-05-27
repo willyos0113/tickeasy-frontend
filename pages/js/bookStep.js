8fd317bf3848e96aecf609d5600c5d3b43cbe18b
yiwei <willyos0113@gmail.com>
2025-05-28 01:33:47 +0800

    購票頁: 動態新增組件

--- pages/js/bookStep.js
+++ pages/js/bookStep.js
@@ -14,17 +14,35 @@ $(".back").on("click", () => {
 $(".next").on("click", () => {
   location.href = "bookStep2.html";
 });
-
-$(".subtract").on("click", (e) => {
-  $(e.target);
-  let count = $(".count").val();
+$(".substract").on("click", (e) => {
+  const control = $(e.target).parent();
+  const input = control.next().find("input");
+  let count = input.val();
   if (count > 0) {
     count--;
-    $(".count").val(count);
+    input.val(count);
   }
 });
-$(".add").on("click", () => {
-  let count = $(".count").val();
+$(".add").on("click", (e) => {
+  const control = $(e.target).parent();
+  const input = control.prev().find("input");
+  let count = input.val();
   count++;
-  $(".count").val(count);
+  input.val(count);
+  console.log(input.val());
 });
+// fetch() 票種表
+let ticketTypeLst;
+fetch("http://localhost:8080/maven-tickeasy-v1/buy/ticket-types?eventId=1")
+  .then((resp) => resp.json())
+  .then((ticketTypes) => {
+    ticketTypeLst = ticketTypes;
+  });
+// fetch() 票種欄
+fetch("./includes/typeComponent.html")
+  .then((resp) => resp.text())
+  .then((htmlContent) => {
+    for (let i = 0; i < 5; i++) {
+      $(".type-container").append(htmlContent);
+    }
+  });
