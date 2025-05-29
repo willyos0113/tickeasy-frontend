09eeeb710ea5b97526c0873f0bd9a6cabb84c107
yiwei <willyos0113@gmail.com>
2025-05-29 22:55:15 +0800

    購票頁: 調整資料夾，區分 layout 為共通部分、ui 為各自部分

--- pages/js/bookStep.js
+++ pages/js/bookStep.js
@@ -4,19 +4,20 @@ $(".back").on("click", () => {
   location.href = "https://www.google.com";
 });
 $(".next").on("click", () => {
-  // post 東西出去...
-  const formValues = $(".count")
+  // post 東西出去(under constructing...)
+  const inputsValues = $(".count")
     .map((index, el) => {
       const coreParent = $(el).closest(".level");
       const typeName = coreParent.find(".type-name").text();
       const typePrice = coreParent.find(".type-price").text();
+      // 模擬 json 格式
       return {
-        "type-count": $(el).val(),
-        "type-name": typeName,
-        "type-price": typePrice,
+        typeCount: $(el).val(),
+        typeName: typeName,
+        typePrice: typePrice,
       };
     })
     .get();
-  console.log(formValues);
+  console.log(inputsValues);
   // location.href = "bookStep2.html";
 });
